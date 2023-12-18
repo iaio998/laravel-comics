ed7313a99bae76f586bdaafb0095e5fb803cf9eb
iaio998 <depalatisiacopo@gmail.com>
2023-12-18 14:55:06 +0100

    Added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,27 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<div>
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-2">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo">
+            </div>
+            <div class="col">
+                <ul class="d-flex align-items-center justify-content-center m-0">
+                    <li class="px-1"><a href="">Characters</a></li>
+                    <li class="px-1"><a href="">Comics</a></li>
+                    <li class="px-1"><a href="">Movies</a></li>
+                    <li class="px-1"><a href="">TV</a></li>
+                    <li class="px-1"><a href="">Games</a></li>
+                    <li class="px-1"><a href="">Collectibles</a></li>
+                    <li class="px-1"><a href="">Videos</a></li>
+                    <li class="px-1"><a href="">Fans</a></li>
+                    <li class="px-1"><a href="">News</a></li>
+                    <li class="px-1"><a href="">Shop</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <p class="m-0">Search...</p>
+            </div>
+        </div>
+    </div>
+    <div id="jumbotron"></div>
+</div>
